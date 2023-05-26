d203c5b01a1dc202a8093bbfe1f1186cf5320421
DESKTOP-O1J2CJL\admin <ntd113663@gmail.com>
2023-05-26 16:04:01 +0700

    for wile

---
+++ loops/forloops.js
@@ -0,0 +1,10 @@
+
+let arr1 = ['Liver','Chelsea','Ars','MU']
+let i = 0
+while(i < arr1.length){
+    //if else
+    if(arr1[i].length === 2){
+        console.log('Top: ', i + 1, arr1[i]);
+    }
+    i++;
+}
