a038054debecf5a0f21f547feb6a692671f13f36
DESKTOP-O1J2CJL\admin <ntd113663@gmail.com>
2023-05-26 15:06:04 +0700

    different of compare

--- variables/variables.js
+++ variables/variables.js
@@ -8,3 +8,15 @@ let obj = {
 let c = 'strng';
 console.log('name is: ', obj.name)
 console.log('type of b: ', typeof b, 'type of c: ', typeof c);
+
+
+let array = ['a','b','c'];
+console.log(array[1])
+
+
+// === type,value
+// == value
+
+
+
+// empty, null, undefiend
