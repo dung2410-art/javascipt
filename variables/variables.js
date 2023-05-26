915c21a308853a62263263b50c2ce78f566daac1
DESKTOP-O1J2CJL\admin <ntd113663@gmail.com>
2023-05-26 14:17:08 +0700

    object

--- variables/variables.js
+++ variables/variables.js
@@ -1,3 +1,10 @@
 let a = "string a"
 
-console.log(a)
+//key: , value:
+let obj = {
+    name: 'Dung',
+    address: 'Ha Noi'
+}; //object
+let c = 'strng';
+console.log('name is: ', obj.name)
+console.log('type of b: ', typeof b, 'type of c: ', typeof c);
